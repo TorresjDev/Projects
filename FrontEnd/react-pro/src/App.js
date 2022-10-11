715c27321ddfc8a89c9c66513508445e75fcf7e2
Jesus Torres <59432133+JxTorres003@users.noreply.github.com>
2022-10-11 00:13:31 -0700

    added files

--- FrontEnd/react-pro/src/App.js
+++ FrontEnd/react-pro/src/App.js
@@ -1,14 +1,26 @@
 import Navbar from "./Navbar";
+import Create from "./Create";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
 	return (
-		<div className="App">
-			<Navbar />
-			<div className="content">
-				<Home />
+		<Router>
+			<div className="App">
+				<Navbar />
+				<div className="content">
+					<Switch>
+						<Route exact path="/">
+							<Home />
+						</Route>
+						<Route path="/create">
+							<Create />
+						</Route>
+						<Route path="/contact">Contact</Route>
+					</Switch>
+				</div>
 			</div>
-		</div>
+		</Router>
 	);
 }
 
