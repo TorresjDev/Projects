f40de17ea4e982dd1bbb367c652bb90c213067c1
Jesus Torres <59432133+JxTorres003@users.noreply.github.com>
2022-10-20 01:07:19 -0700

    login page frontend

--- FrontEnd/Design/Angular/frontend/src/app/app.module.ts
+++ FrontEnd/Design/Angular/frontend/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { RatingModule } from 'ng-starrating';
 import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { TitleComponent } from './components/partials/title/title.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,8 +26,15 @@ import { TitleComponent } from './components/partials/title/title.component';
     SearchComponent,
     TagsComponent,
     TitleComponent,
+    LoginPageComponent,
   ],
-  imports: [AppRoutingModule, BrowserModule, HttpClientModule, RatingModule],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    RatingModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
