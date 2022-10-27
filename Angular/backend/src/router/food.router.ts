57f3df968c9f76b1688721805a4a27b02155ad03
Jesus Torres <59432133+JxTorres003@users.noreply.github.com>
2022-10-26 23:25:00 -0700

    connecting models to mongoDB

--- Angular/backend/src/router/food.router.ts
+++ Angular/backend/src/router/food.router.ts
@@ -1,23 +1,55 @@
 import { Router } from "express";
 import { sample_foods, sample_tags } from "../data";
+import asyncHandler from "express-async-handler";
+import { FoodModel } from "../models/food.model";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-	res.send(sample_foods);
-});
+router.get(
+	"/seed",
+	asyncHandler(async (req, res) => {
+		const foodsCount = await FoodModel.countDocuments();
+		if (foodsCount > 0) {
+			res.send("Seed is already done!");
+			return;
+		}
 
-router.get("/search/:searchTerm", (req, res) => {
-	const searchTerm = req.params.searchTerm;
-	const foods = sample_foods.filter((food) =>
-		food.name.toLowerCase().includes(searchTerm.toLowerCase()),
-	);
-	res.send(foods);
-});
+		await FoodModel.create(sample_foods);
+		res.send("Seed is done!");
+	}),
+);
 
-router.get("/tags", (req, res) => {
-	res.send(sample_tags);
-});
+router.get(
+	"/",
+	asyncHandler(async (req, res) => {
+		const foods = await FoodModel.find();
+		res.send(foods);
+	}),
+);
+
+router.get(
+	"/search/:searchTerm",
+	asyncHandler(async (req, res) => {
+		const searchRegex = new RegExp(req.params.searchTerm, "i");
+		const foods = await FoodModel.find({ name: { $regex: searchRegex } });
+		res.send(foods);
+	}),
+);
+
+router.get(
+	"/tags",
+	asyncHandler(async (req, res) => {
+		const tags = await FoodModel.aggregate([
+			{ $unwind: "$tags" },
+			{
+				$group: {
+					_id: "$tags",
+					count: { $sum: 1 },
+				},
+			},
+		]);
+	}),
+);
 
 router.get("/tag/:tagName", (req, res) => {
 	const tagName = req.params.tagName;
