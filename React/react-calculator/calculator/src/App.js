276ecfb7aa4a5132bc87efc0b92edbcb424e9e46
Jesus Torres <torres.jesus2143@gmail.com>
2023-02-05 12:31:51 -0800

    calculator output

--- React/react-calculator/calculator/src/App.js
+++ React/react-calculator/calculator/src/App.js
@@ -1,30 +1,20 @@
-import "./App.css";
-import { useState } from "react";
-import Keypad from "./components/Keypad.jsx";
+import "./styles.css";
 
 function App() {
-	const [input, setInput] = useState("");
-	const handleClick = (value) => {
-		setInput(input + value);
-	};
-	const handleEquals = () => {
-		setInput(eval(input).toString());
-	};
-	const handleClear = () => {
-		setInput("");
-	};
-
 	return (
 		<>
-			<div className="App">
-				<div className="calculator">
-					<input type="text" className="display" value={input} readOnly />
-					<Keypad
-						handleClear={handleClear}
-						handleClick={handleClick}
-						handleEquals={handleEquals}
-					/>{" "}
+			<div className="calculator-grid">
+				<div className="output">
+					<div className="previous-operand"> 19 * 3 </div>{" "}
+					<div className="current-operand"> 193 </div>{" "}
 				</div>{" "}
+				<button className="span-two"> AC </button> <button> DEL </button>
+				<button> % </button> <button> 1 </button> <button> 2 </button>{" "}
+				<button> 3 </button> <button> * </button> <button> 4 </button>{" "}
+				<button> 5 </button> <button> 6 </button> <button> + </button>{" "}
+				<button> 7 </button> <button> 8 </button> <button> 9 </button>{" "}
+				<button> - </button> <button> . </button> <button> 0 </button>{" "}
+				<button className="span-two">= </button>{" "}
 			</div>{" "}
 		</>
 	);
