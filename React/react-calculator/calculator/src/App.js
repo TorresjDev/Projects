b7be75bee571634643bbbb3dfbbdb65ad9f54660
Jesus Torres <torres.jesus2143@gmail.com>
2023-02-05 11:56:46 -0800

    working on react calculator

--- React/react-calculator/calculator/src/App.js
+++ React/react-calculator/calculator/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Keypad from "./components/Keypad.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [input, setInput] = useState("");
+	const handleClick = (value) => {
+		setInput(input + value);
+	};
+	const handleEquals = () => {
+		setInput(eval(input).toString());
+	};
+	const handleClear = () => {
+		setInput("");
+	};
+
+	return (
+		<>
+			<div className="App">
+				<div className="calculator">
+					<input type="text" className="display" value={input} readOnly />
+					<Keypad
+						handleClear={handleClear}
+						handleClick={handleClick}
+						handleEquals={handleEquals}
+					/>{" "}
+				</div>{" "}
+			</div>{" "}
+		</>
+	);
 }
 
 export default App;
