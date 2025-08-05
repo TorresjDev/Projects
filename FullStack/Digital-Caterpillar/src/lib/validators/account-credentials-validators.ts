d668db51c9bdce276ccc2c1c7d6a048da29ac43f
TorresjDev <j.torres3.dev@gmail.com>
2025-08-05 06:40:37 -0500

    Remove squashed Digital-Caterpillar to prepare for proper history-preserving merge

--- FullStack/Digital-Caterpillar/src/lib/validators/account-credentials-validators.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const AuthCredentialsValidator = z.object({
-	email: z.string().email(),
-	password: z.string().min(9, { message: "Invalid password, must be at least 8 characters long" })
-});
-
-export type TAuthCredentialsValidator = z.infer<typeof AuthCredentialsValidator>;
